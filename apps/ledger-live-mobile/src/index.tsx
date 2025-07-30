0b54f5f44258965cb27ae03cc342fd2db5533146
fAnselmi-Ledger <francesco.anselmi@ledger.fr>
2025-07-30 18:04:58 +0200

    feat(signer-solana): Add DMK signer to Solana signer

--- apps/ledger-live-mobile/src/index.tsx
+++ apps/ledger-live-mobile/src/index.tsx
@@ -113,6 +113,7 @@ import getOrCreateUser from "./user";
 import { FIRST_PARTY_MAIN_HOST_DOMAIN } from "./utils/constants";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import useNativeStartupInfo from "./hooks/useNativeStartupInfo";
+import { setSolanaLdmkEnabled } from "@ledgerhq/live-common/families/solana/setup";
 
 if (Config.DISABLE_YELLOW_BOX) {
   LogBox.ignoreAllLogs();
@@ -152,6 +153,7 @@ function App() {
   const dispatch = useDispatch();
   const isTrackingEnabled = useSelector(trackingEnabledSelector);
   const automaticBugReportingEnabled = useSelector(reportErrorsEnabledSelector);
+  const ldmkSolanaSignerFeatureFlag = useFeature("ldmkSolanaSigner");
   useNativeStartupInfo();
 
   const datadogAutoInstrumentation: AutoInstrumentationConfiguration = useMemo(
@@ -169,6 +171,12 @@ function App() {
     }),
     [datadogFF?.params, automaticBugReportingEnabled],
   );
+
+  useEffect(() => {
+    if (typeof ldmkSolanaSignerFeatureFlag?.enabled === "boolean") {
+      setSolanaLdmkEnabled(ldmkSolanaSignerFeatureFlag?.enabled);
+    }
+  }, [ldmkSolanaSignerFeatureFlag]);
 
   useEffect(() => {
     if (providerNumber && isLDMKEnabled) {
