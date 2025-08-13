ae9ec395f1e7257dd81203c2e2b0d28c815b757a
RobinVncnt <robin.vincent@ledger.fr>
2025-08-13 16:51:42 +0200

    feat: LLM MAD add balance in asset selection

--- apps/ledger-live-mobile/src/newArch/features/ModularDrawer/screens/AssetSelection/index.tsx
+++ apps/ledger-live-mobile/src/newArch/features/ModularDrawer/screens/AssetSelection/index.tsx
@@ -16,6 +16,9 @@ import {
   useBottomSheet,
 } from "@gorhom/bottom-sheet";
 import { AssetsEmptyList } from "LLM/components/EmptyList/AssetsEmptyList";
+import { useGroupedCurrenciesByProvider } from "@ledgerhq/live-common/deposit/useGroupedCurrenciesByProvider.hook";
+import { LoadingBasedGroupedCurrencies } from "@ledgerhq/live-common/deposit/type";
+import { useRightBalanceModule } from "./modules/useRightBalanceModule";
 
 export type AssetSelectionStepProps = {
   isOpen: boolean;
@@ -47,6 +50,9 @@ const AssetSelection = ({
   const { shouldHandleKeyboardEvents } = useBottomSheetInternal();
   const { collapse } = useBottomSheet();
   const listRef = useRef<FlatList>(null);
+  const { result } = useGroupedCurrenciesByProvider(true) as LoadingBasedGroupedCurrencies;
+
+  const enrichedAssets = useRightBalanceModule(availableAssets, result.currenciesByProvider);
 
   const handleAssetClick = useCallback(
     (asset: AssetType) => {
@@ -136,7 +142,7 @@ const AssetSelection = ({
       <BottomSheetVirtualizedList
         ref={listRef}
         scrollToOverflowEnabled={true}
-        data={itemsToDisplay}
+        data={enrichedAssets}
         keyExtractor={item => item.id}
         getItemCount={itemsToDisplay => itemsToDisplay.length}
         getItem={(itemsToDisplay, index) => itemsToDisplay[index]}
