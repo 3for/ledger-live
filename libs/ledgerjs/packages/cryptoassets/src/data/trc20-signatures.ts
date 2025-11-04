b180e286d1af467ac52d916bb1d2da45a35519cb
3for <287494524@qq.com>
2025-11-04 17:55:50 +0800

    refactor trc20 signatures for tip712

---
+++ libs/ledgerjs/packages/cryptoassets/src/data/trc20-signatures.ts
@@ -0,0 +1,9 @@
+import TRC20Signatures from "./trc20-signatures.json";
+
+export { default as hash } from "./erc20-signatures-hash.json";
+
+export default TRC20Signatures;
+
+export const signatures = {
+  1151668124: TRC20Signatures,
+};
